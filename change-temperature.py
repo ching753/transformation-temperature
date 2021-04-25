626a6aaa2cda440720f205214e29eaf603560b26
ching753 <vickyvicky987@yahoo.com.tw>
2021-04-25 19:47:34 +0800

    1.Simplify user input. 2.Modified conversion formula

--- change-temperature.py
+++ change-temperature.py
@@ -1,20 +1,23 @@
 print ('這是一個華氏/攝氏溫度轉換程式')
+
 while True :
+	print('華氏 : f','攝氏 : c', 'End : e')
 	U = input('請輸入你想知道的溫度單位: ')
-	if U != '華氏' and U !='攝氏' and U != 'close' :
-		print ('輸入錯誤!!請輸入華氏 or 攝氏 or close')
+	
+	if U != 'f' and U !='c' and U != 'e' :
+		print ('輸入錯誤!!請輸入 "f" or "c" or "e"')
 	else :
-		if U =='close' :
+		if U =='e' :
 			print ('謝謝使用')
 			break
 		else :
 			T = input('請輸入要轉換的溫度: ')
 			T = float(T)
-			if U =='華氏' :
-				W = T*9/5-32
-				print ('等於華氏', W, '度')
+			if U =='f' :
+				Fahrenheit = T*9/5+32
+				print ('等於華氏', Fahrenheit, '度')
 			else :
-				C = (T-32)*5/9
-				print ('等於攝氏', C, '度')
+				Celsius = (T-32)*5/9
+				print ('等於攝氏', Celsius, '度')
 
 
